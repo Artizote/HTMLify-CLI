403f313107356857ae38cbf0e661594e9c8e0f83
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-03-03 22:35:23 +0530

    Add BUILD_TYPE flag for building DEV and PROD

---
+++ include/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+//
+// DEV_ENV -> Development Environment
+// PROD_ENV -> Production Environment
+//
+
+#ifdef DEV_ENV
+    #define API_URL_BASE "http://api.localhost:8000"
+#endif
+
+#ifdef PROD_ENV
+    #define API_URL_BASE "https://api.htmlify.me"
+#endif
+
+#endif
