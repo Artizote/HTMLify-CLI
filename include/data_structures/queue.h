913c7d18f92f91880fe81ff992c541d4d6e13be9
Aman Babu Hemant <amanbabuhemant@gmail.com>
2026-08-27 04:00:44 +0530

    Add Queue data structure

---
+++ include/data_structures/queue.h
@@ -0,0 +1,54 @@
+// Queue data structure
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+/*
+ * Dynamic Generic Queue
+ *
+ * Shrinks and Expands as per number
+ * of items present in queue
+ */
+typedef struct {
+    size_t cap;
+    size_t used;
+    void **items;
+} Queue;
+
+
+// Create new `Queue` struct
+Queue *Queue_new(void);
+
+// Initlize `queue` with cap
+void Queue_init(Queue *queue, size_t cap);
+
+// Return an initlized `Queue`, with capicity of 16
+Queue *Queue_create(void);
+
+// Add `item` to `queue`
+void Queue_push(Queue *queue, void *item);
+
+// Get first item from `queue` or NULL if `queue` is empty
+void *Queue_get(Queue *queue);
+
+// Determine if `queue` capicity should expand
+bool Queue_should_expand(Queue *queue);
+
+// Determine if `queue` capicity should shrink
+bool Queue_should_shrink(Queue *queue);
+
+// Expend the Queue capacity by ~1.5 factor
+void Queue_expand(Queue *queue);
+
+// Shrink the Queue capacity
+void Queue_shrink(Queue *queue);
+
+// Check if `queue` is empty
+bool Queue_is_empty(Queue *queue);
+
+// Check if `queue` is full
+bool Queue_is_full(Queue *queue);
+
+// Free the `Queue`
+void Queue_free(Queue *queue);
+
---
+++ src/data_structures/queue.c
@@ -0,0 +1,77 @@
+// Queue data strructure
+
+#include "data_structures/queue.h"
+
+
+Queue *Queue_new(void) {
+    Queue *queue = malloc(sizeof(Queue));
+    return queue;
+}
+
+void Queue_init(Queue *queue, size_t cap) {
+    queue->cap = cap;
+    queue->used = 0;
+    queue->items = malloc(cap * sizeof(void*));
+}
+
+Queue *Queue_create(void) {
+    Queue *queue = Queue_new();
+    Queue_init(queue, 16);
+    return queue;
+}
+
+void Queue_push(Queue *queue, void *item) {
+    if (Queue_should_expand(queue)) {
+        Queue_expand(queue);
+    }
+    queue->items[queue->used++] = item;
+}
+
+void *Queue_get(Queue *queue) {
+    if (Queue_is_empty(queue)) {
+        return NULL;
+    }
+    if (Queue_should_shrink(queue)) {
+        Queue_shrink(queue);
+    }
+    void *item = queue->items[0];
+    for (size_t i = 0; i < queue->used-1; i++) {
+        queue->items[i] = queue->items[i+1]; 
+    }
+    queue->used--;
+    return item;
+}
+
+bool Queue_should_expand(Queue *queue) {
+    return queue->used >= queue->cap * 0.95;
+}
+
+bool Queue_should_shrink(Queue *queue) {
+    return queue->used <= queue->cap * 0.30;
+}
+
+void Queue_expand(Queue *queue) {
+    size_t new_cap = queue->cap * 1.5 + 1;
+    queue->items = realloc(queue->items, new_cap * sizeof(void *));
+    queue->cap = new_cap;
+}
+
+void Queue_shrink(Queue *queue) {
+    size_t new_cap = queue->cap * 0.5 + 1;
+    queue->items = realloc(queue->items, new_cap * sizeof(void *));
+    queue->cap = new_cap;
+}
+
+bool Queue_is_empty(Queue *queue) {
+    return queue->used == 0;
+}
+
+bool Queue_is_full(Queue *queue) {
+    return queue->cap == queue->used;
+}
+
+void Queue_free(Queue *queue) {
+    free(queue->items);
+    free(queue);
+}
+
